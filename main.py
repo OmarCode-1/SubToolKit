e4b92c5d4d41787de44edb7ddbae502a6a8651dc
Omar Ibrahim <omar.programming.0.0122@gmail.com>
2026-02-19 16:03:35 +0200

    V1.0.4

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from Modules.SubDomains import *
 import sys
+from colorama import Fore, Style, init
 
 
+
+banner = r"""
+{cyan}======================================================================
+{yellow}   ____        _    _______    _ _    _ _ _ _ _   
+{yellow}  / ___| _   _| |__|__  / |__| | | _(_) | (_) |_ 
+{yellow}  \___ \| | | | '_ \ / /| '_ \ | |/ / | | | __|
+{yellow}   ___) | |_| | |_) / /_| | | |   <| | | | |_ 
+{yellow}  |____/ \__,_|_.__/____|_| |_|_|\_\_|_|_|\__|
+{magenta}
+               SubToolKit
+               Coded by Omar Ibrahim
+               CTF: 0x4FMR
+{cyan}======================================================================
+""".format(cyan=Fore.CYAN, yellow=Fore.YELLOW, magenta=Fore.MAGENTA)
 
 if __name__ == '__main__':
+    init(autoreset=True)
+    print(banner)
+
     if len(sys.argv) != 2:
         print("Usage: python main.py domain.com")
         sys.exit(1)
@@ -12,4 +30,4 @@ if __name__ == '__main__':
 
     print("Target domain: ", domain)
     print("Starting Recon: ", domain)
-    program = SubToolKit(f"{domain}")
+    # SubToolKit(f"{domain}")
